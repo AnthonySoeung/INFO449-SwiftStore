f121b83acb96e796e99ba8cd659f57683edac8fd
anthonysoeung <asoeung@uw.edu>
2026-01-27 17:45:24 -0800

    finished store

--- Store/Store/main.swift
+++ Store/Store/main.swift
@@ -7,13 +7,82 @@
 
 import Foundation
 
-protocol SKU {}
+protocol SKU {
+    var name: String {get};
+    func price() -> Int;
+}
 
-class Item {}
+class Item: SKU {
+    let name: String;
+    private let unitPrice: Int;
 
-class Receipt {}
+    init(name: String, priceEach: Int) {
+        self.name = name;
+        self.unitPrice = priceEach;
+    }
 
-class Register {}
+    func price() -> Int {
+        return unitPrice;
+    }
+}
+
+
+class Receipt {
+    private var itemsScanned: [SKU] = [];
+    
+    func add(_ item: SKU) {
+        itemsScanned.append(item);
+    }
+    func items() -> [SKU] {
+        return itemsScanned;
+    }
+    func subtotal() -> Int {
+        var total = 0;
+        for item in itemsScanned {
+            total = total + item.price();
+        }
+        return total;
+    }
+    func output() -> String {
+        var result = "";
+        result += "Receipt:\n";
+
+        for item in itemsScanned {
+            let dollars = Double(item.price()) / 100.0
+            result += "\(item.name): $\(String(format: "%.2f", dollars))\n";
+        }
+
+        result += "------------------\n";
+
+        let totalDollars = Double(subtotal()) / 100.0
+        result += "TOTAL: $\(String(format: "%.2f", totalDollars))";
+
+        return result;
+    }
+
+
+    func total() -> Int {
+        return subtotal();
+    }
+}
+
+class Register {
+    private var receipt: Receipt;
+    init() {
+        self.receipt = Receipt();
+    }
+    func scan(_ sku: SKU) {
+        receipt.add(sku);
+    }
+    func subtotal() -> Int {
+        return receipt.subtotal();
+    }
+    func total() -> Receipt {
+        let finishedReceipt = receipt;
+        receipt = Receipt();
+        return finishedReceipt;
+    }
+}
 
 class Store {
     let version = "0.1"
